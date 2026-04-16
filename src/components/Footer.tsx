2e4d618f2e26e380e9a23cb9ee5476b72e74c3a0
Vincent S <esyfer@free.fr>
2026-04-16 15:13:56 +0100

    lien footer x

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,12 @@ const Footer = () => {
     return (
         <footer className="flex justify-center items-center mb-2 text-gray-400 text-sm">
             © 2026- by{' '}
-            <a href="mailto:vincent@vindows.dev" className="italic">
+            <a
+                href="https://x.com/vynsedev"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="ml-1 italic hover:text-white transition-colors"
+            >
                 Vynse.
             </a>
         </footer>
